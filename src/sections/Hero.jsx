d5902e856185f87858a0e4cc9b512680e0107875
Tejas <tejasbobade2002@gmail.com>
2025-01-27 14:53:17 +0530

    Completed a about section dowaloaded 3D model react-globe.gl using terminal.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -102,8 +102,8 @@ const Hero = () => {
         </div>
 
         <div className='absolute bottom-7 left-0 right-0 w-full z-10 c-space'>
-            <a href="#contact" className='w-fit'>
-                <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96"/>
+            <a href="/" className='w-fit'>
+                <Button name="Download CV" isBeam containerClass="sm:w-fit w-full sm:min-w-96"/>
             </a>
         </div>
     </section>
