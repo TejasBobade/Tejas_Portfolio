17ba143b7a39237ea22e6974ed208b1edc5ff425
Tejas <tejasbobade2002@gmail.com>
2025-01-27 07:29:21 +0530

    Added Public Folder And completed Hero section main 3D image.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Hero from './sections/Hero'
+import Navbar from './sections/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main className='max-w-7xl mx-auto'>
-      <h1 className='text-2xl text-white underline'>Hello, Three.js</h1>
+      <Navbar />
+      <Hero />
     </main>
   )
 }
---
+++ src/components/Target.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// 2.20
+const Target = () => {
+  return (
+    <div>Target</div>
+  )
+}
+
+export default Target
