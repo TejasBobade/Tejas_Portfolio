82129aa90eb161979d9203562c2d269193ad63af
Tejas <tejasbobade2002@gmail.com>
2025-02-03 08:50:23 +0530

    Added a new project changed project order and solved the contact form error.

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,136 +21,48 @@ export const navLinks = [
     },
   ];
   
-  export const clientReviews = [
-    {
-      id: 1,
-      name: 'Emily Johnson',
-      position: 'Marketing Director at GreenLeaf',
-      img: 'assets/review1.png',
-      review:
-        'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
-    },
-    {
-      id: 2,
-      name: 'Mark Rogers',
-      position: 'Founder of TechGear Shop',
-      img: 'assets/review2.png',
-      review:
-        'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
-    },
-    {
-      id: 3,
-      name: 'John Dohsas',
-      position: 'Project Manager at UrbanTech ',
-      img: 'assets/review3.png',
-      review:
-        'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
-    },
-    {
-      id: 4,
-      name: 'Ether Smith',
-      position: 'CEO of BrightStar Enterprises',
-      img: 'assets/review4.png',
-      review:
-        'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
-    },
-  ];
+  // export const clientReviews = [
+  //   {
+  //     id: 1,
+  //     name: 'Emily Johnson',
+  //     position: 'Marketing Director at GreenLeaf',
+  //     img: 'assets/review1.png',
+  //     review:
+  //       'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Mark Rogers',
+  //     position: 'Founder of TechGear Shop',
+  //     img: 'assets/review2.png',
+  //     review:
+  //       'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'John Dohsas',
+  //     position: 'Project Manager at UrbanTech ',
+  //     img: 'assets/review3.png',
+  //     review:
+  //       'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Ether Smith',
+  //     position: 'CEO of BrightStar Enterprises',
+  //     img: 'assets/review4.png',
+  //     review:
+  //       'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+  //   },
+  // ];
   
   export const myProjects = [
-    {
-      title: 'Brainwave: Modern Landing Page For SAAS',
-      desc: 'Brainwave is a cutting-edge SaaS landing page designed to create modern, visually captivating websites. With sleek parallax effects and bento box layouts, it delivers a stylish, mobile-first user experience while enhancing interactivity and engagement.',
-      subdesc:
-        'Built with React.js, Tailwind CSS, and Framer Motion, Brainwave combines smooth animations with a clean, scalable UI, ensuring high performance and responsiveness across all devices.',
-      href: 'https://brainwave-nine-ashen.vercel.app/',
-      texture: '/textures/project/Pro2.mp4',
-      logo: '/assets/project-logo/Pro2.svg',
-      logoStyle: {
-        backgroundColor: '#13202F',
-        border: '0.2px solid #17293E',
-        boxShadow: '0px 0px 60px 0px #2F6DB54D',
-      },
-      spotlight: '/assets/spotlight2.png',
-      tags: [
-        {
-          id: 1,
-          name: 'React.js',
-          path: '/assets/react.svg',
-        },
-        {
-          id: 2,
-          name: 'TailwindCSS',
-          path: 'assets/tailwindcss.png',
-        },
-      ],
-    },
-    {
-      title: 'VirtualR: Modern Landing Page For Virtual Reality Startup',
-      desc: 'VirtualR is a sleek and immersive landing page for virtual reality startups, designed to captivate users with a modern, engaging experience. Featuring a responsive navbar, gradient hero section, autoplay videos, and a structured features showcase, it highlights the innovation behind VR technology.',
-      subdesc:
-        'Built with Vite, Tailwind CSS, and Framer Motion, VirtualR delivers smooth animations, a clean UI, and a mobile-first design. With a pricing section, testimonials, and seamless deployment on Vercel, it ensures high performance and scalability.',
-      href: 'https://virtual-r-sooty-three.vercel.app/',
-      texture: '/textures/project/Pro3.mp4',
-      logo: '/assets/project-logo/Pro3.png',
-      logoStyle: {
-        backgroundColor: '#60f5a1',
-        background:
-          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
-        border: '0.2px solid rgba(208, 213, 221, 1)',
-        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
-      },
-      spotlight: '/assets/spotlight3.png',
-      tags: [
-        {
-          id: 1,
-          name: 'React.js',
-          path: '/assets/react.svg',
-        },
-        {
-          id: 2,
-          name: 'TailwindCSS',
-          path: 'assets/tailwindcss.png',
-        },
-      ],
-    },
-    {
-      title: 'Tejas-Portfolio',
-      desc: 'Build 3D websites from scratch with Three.js, creating a visually stunning and interactive portfolio. Bring your projects to life with animated 3D models, dynamic avatars, and immersive scenes that react to user interactions.',
-      subdesc:
-        'Built with Three.js, React Three Fiber, Drei, and Vite, this project features a Hero Section with animated 3D objects, a responsive Navbar, Bento Grid, and a Work Section with hover-triggered avatar animations. A seamless deployment setup ensures a high-performance, professional-grade experience.',
-      href: 'https://tejas-portfolio-blush.vercel.app/',
-      texture: '/textures/project/Pro4.mp4',
-      logo: '/assets/project-logo/Pro4.png',
-      logoStyle: {
-        backgroundColor: '#0E1F38',
-        border: '0.2px solid #0E2D58',
-        boxShadow: '0px 0px 60px 0px #2F67B64D',
-      },
-      spotlight: '/assets/spotlight4.png',
-      tags: [
-        {
-          id: 1,
-          name: 'React.js',
-          path: '/assets/react.svg',
-        },
-        {
-          id: 2,
-          name: 'TailwindCSS',
-          path: 'assets/tailwindcss.png',
-        },
-        {
-          id: 3,
-          name: 'Framer Motion',
-          path: '/assets/framer.png',
-        },
-      ],
-    },
     {
       title: 'Snapgram: A Modern Social Media App',
       desc: 'Snapgram is a cutting-edge social media platform designed for seamless user engagement and a native mobile experience. With real-time interactions, infinite scroll, and a sleek UI, it redefines the way users connect and share content.',
       subdesc:
         'Developed with React JS, Appwrite, Tailwind CSS, JavaScript, and Shadcn UI, Snapgram delivers high performance, real-time updates, and an intuitive design optimized for scalability and responsiveness.',
-      href: 'https://insta-void.vercel.app/',
+      href: 'https://insta-void.vercel.app',
       texture: '/textures/project/Pro1.mp4',
       logo: '/assets/project-logo/Pro1.svg',
       logoStyle: {
@@ -180,8 +92,185 @@ export const navLinks = [
           name: 'Framer Motion',
           path: '/assets/framer.png',
         },
+        {
+          id: 5,
+          name: 'Appwrite',
+          path: '/assets/appwrite.png',
+        },
+        {
+          id: 6,
+          name: 'Shadcn UI',
+          path: '/assets/shadcn.png',
+        },
       ],
     },
+    {
+      title: 'StableMax - Text to Image Generator For Free',
+      desc: 'StableMax is a free text-to-image generator built with Next.js, offering fast, secure, and scalable image generation. It leverages server-side rendering, Prisma with Postgres (neonDB), and Framer Motion animations, providing a seamless and efficient user experience.',
+      subdesc:
+        'Developed using Next.js, TypeScript, Tailwind CSS, Prisma, and Shadcn UI, StableMax ensures optimized image processing, easy authentication with NextAuth, and responsive UI components. With Vercel deployment, it offers a highly maintainable and secure platform for effortless text-to-image conversion.',
+      href: 'https://stable-max-green.vercel.app',
+      texture: '/textures/project/Pro2.mp4',
+      logo: '/assets/project-logo/Pro2.png',
+      logoStyle: {
+        backgroundColor: '#2A1816',
+        border: '0.2px solid #36201D',
+        boxShadow: '0px 0px 60px 0px #AA3C304D',
+      },
+      spotlight: '/assets/spotlight1.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'Next.js',
+          path: '/assets/next.png',
+        },
+        {
+          id: 3,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 4,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 5,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+        {
+          id: 6,
+          name: 'Prisma',
+          path: '/assets/prisma.png',
+        },
+        {
+          id: 7,
+          name: 'Shadcn UI',
+          path: '/assets/shadcn.png',
+        },
+        
+      ],
+    },
+    {
+      title: 'Brainwave: Modern Landing Page For SAAS',
+      desc: 'Brainwave is a cutting-edge SaaS landing page designed to create modern, visually captivating websites. With sleek parallax effects and bento box layouts, it delivers a stylish, mobile-first user experience while enhancing interactivity and engagement.',
+      subdesc:
+        'Built with React.js, Tailwind CSS, and Framer Motion, Brainwave combines smooth animations with a clean, scalable UI, ensuring high performance and responsiveness across all devices.',
+      href: 'https://brainwave-nine-ashen.vercel.app/',
+      texture: '/textures/project/Pro3.mp4',
+      logo: '/assets/project-logo/Pro3.svg',
+      logoStyle: {
+        backgroundColor: '#13202F',
+        border: '0.2px solid #17293E',
+        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'VirtualR: Modern Landing Page For Virtual Reality Startup',
+      desc: 'VirtualR is a sleek and immersive landing page for virtual reality startups, designed to captivate users with a modern, engaging experience. Featuring a responsive navbar, gradient hero section, autoplay videos, and a structured features showcase, it highlights the innovation behind VR technology.',
+      subdesc:
+        'Built with Vite, Tailwind CSS, and Framer Motion, VirtualR delivers smooth animations, a clean UI, and a mobile-first design. With a pricing section, testimonials, and seamless deployment on Vercel, it ensures high performance and scalability.',
+      href: 'https://virtual-r-sooty-three.vercel.app/',
+      texture: '/textures/project/Pro4.mp4',
+      logo: '/assets/project-logo/Pro4.png',
+      logoStyle: {
+        backgroundColor: '#60f5a1',
+        background:
+          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'Tejas-Portfolio',
+      desc: 'Build 3D websites from scratch with Three.js, creating a visually stunning and interactive portfolio. Bring your projects to life with animated 3D models, dynamic avatars, and immersive scenes that react to user interactions.',
+      subdesc:
+        'Built with Three.js, React Three Fiber, Drei, and Vite, this project features a Hero Section with animated 3D objects, a responsive Navbar, Bento Grid, and a Work Section with hover-triggered avatar animations. A seamless deployment setup ensures a high-performance, professional-grade experience.',
+      href: 'https://tejas-portfolio-blush.vercel.app/',
+      texture: '/textures/project/Pro5.mp4',
+      logo: '/assets/project-logo/Pro5.png',
+      logoStyle: {
+        backgroundColor: '#0E1F38',
+        border: '0.2px solid #0E2D58',
+        boxShadow: '0px 0px 60px 0px #2F67B64D',
+      },
+      spotlight: '/assets/spotlight4.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+        {
+          id: 4,
+          name: 'Three.js',
+          path: '/assets/three.png',
+        },
+        {
+          id: 5,
+          name: 'React Three Fiber',
+          path: '/assets/rtf.png',
+        },
+        {
+          id: 6,
+          name: 'Shadcn UI',
+          path: '/assets/shadcn.png',
+        },
+      ],
+    },
+    
   ];
   
   export const calculateSizes = (isSmall, isMobile, isTablet) => {
